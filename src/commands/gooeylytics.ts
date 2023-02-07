d67551485e2a64ebde90ee88343827625945671e
MrTuxedo <enderhex@protonmail.com>
2023-02-07 12:11:36 -0800

    add a couple of commands, also now using embeds

--- src/commands/gooeylytics.ts
+++ src/commands/gooeylytics.ts
@@ -1,7 +1,30 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { EmbedBuilder, SlashCommandBuilder, ChatInputCommandInteraction, APIEmbedField, APIEmbed } from 'discord.js';
 
 import { fetchTokenCollection, fetchSnapshotTimestamp } from '../utility';
-import { findAndSortByETHGobbled, findUnburiedDead } from '../transformations';
+import { findAndSortByETHGobbled, findExtinctBodyTypes, findUnburiedDead } from '../transformations';
+import { Gooey } from '../../types/gooey';
+
+const mkUnburiedEmbed = (list: number[]) =>
+  new EmbedBuilder()
+    .setTitle(`${list.length} unburied Gooeys with 0 health`)
+    .setDescription(list.join(' '))
+    .setImage('https://ethgobblers.com/bury-icon.svg');
+
+const mkMCLeaderboardEmbed = (list: Gooey[]) =>
+  new EmbedBuilder()
+    .setTitle(`Top 25 Gooey Leaderboard by ETH Gobbled`)
+    .setImage('https://ethereum.org/static/655aaefb744ae2f9f818095a436d38b5/e1ebd/eth-diamond-purple-purple.png')
+    .addFields(...list.map(({name, ethGobbled}, i) => ({
+      name: `${i + 1}) ${name}`,
+      value: `${ethGobbled} ETH`,
+      inline: true
+    })));
+
+const mkExtinctionEmbed = (list: string[]) =>
+    new EmbedBuilder()
+      .setTitle(`Found ${list.length} extinct gooey types`)
+      .setDescription(list.join('\n'));
+
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,44 +38,52 @@ module.exports = {
 
     .addSubcommand(subcommand =>
       subcommand
-      .setName('unburied')
-      .setDescription('Print a list of all Gooeys at zero HP that have not been buried'))
+        .setName('unburied')
+        .setDescription('Print a list of all Gooeys at zero health that have not been buried'))
     
-      .addSubcommand(subcommand =>
-        subcommand
-          .setName('leaderboard')
-          .setDescription('Top gooeys by ETHGobbled')),
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('leaderboard')
+        .setDescription('Top gooeys by ETHGobbled'))
+
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('extinct-types')
+        .setDescription('Print a list of all gooey body types with no living members')),
+    
 
   async execute(interaction: ChatInputCommandInteraction) {
     const subcommand = interaction.options.getSubcommand();
     const gooeys = await fetchTokenCollection()
 
-    switch (subcommand) {
-      case 'updated-at':
+
+      if (subcommand == 'updated-at') {
         const rawTimestamp =  await fetchSnapshotTimestamp();
-        const snapshotTimestamp = new Date(rawTimestamp).getDate();
+        const snapshotTimestamp = new Date(rawTimestamp);
     
-        await interaction.reply(`Gooey database last updated at ${snapshotTimestamp}`)
-        break;
+        return await interaction.reply(`Gooey database last updated at ${snapshotTimestamp.toUTCString()}`);
 
-      case 'unburied':
+      } else if (subcommand == 'unburied') {
         const unburiedDead = findUnburiedDead(gooeys)
+        const unburiedEmbed = mkUnburiedEmbed(unburiedDead)
 
-        await interaction.reply(`Found ${unburiedDead.length} unburied dead \n${unburiedDead}`)
-        break;
+        return await interaction.reply({ embeds: [unburiedEmbed] });
 
-      case 'leaderboard':
-        const top25ETHGobbled = findAndSortByETHGobbled(gooeys).slice(0, 24)
-          .map(({tokenID, ethGobbled}) => `#${tokenID}: ${ethGobbled} ETH\n`)
+      } else if (subcommand == 'leaderboard') {
+        const top25ETHGobbled = findAndSortByETHGobbled(gooeys).slice(0, 24);
+        const leaderboardEmbed = mkMCLeaderboardEmbed(top25ETHGobbled);
 
-        await interaction.reply(`TOP 25 GOOEY by ETH Gobbled\n${top25ETHGobbled}`)
-        break;
-  
-  
-      default:
-        await interaction.reply(`Could not find command for subcommand "${subcommand}"`)
-        break;
-    }
+        return await interaction.reply({ embeds: [leaderboardEmbed] });
+
+      } else if (subcommand == 'extinct-types') {
+        const extinctBodyTypes = findExtinctBodyTypes(gooeys);
+        const extinctionEmbed = mkExtinctionEmbed(extinctBodyTypes);
+
+        return await interaction.reply({ embeds: [extinctionEmbed] });
+
+      } else {
+        return await interaction.reply(`Could not find command for subcommand "${subcommand}"`);
+      }
+
   }
-
 }
