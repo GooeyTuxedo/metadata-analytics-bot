9744853056e71cfb814e038662ca022e442747fd
MrTuxedo <enderhex@protonmail.com>
2023-02-10 16:23:39 -0800

    put the db back in the filesystem

--- src/database.ts
+++ src/database.ts
@@ -7,7 +7,7 @@ dotenv.config();
 import { Gooey, FlatGooey, RawGooey } from "../types/gooey";
 import { replaceAtIdx } from './utility';
 
-export const db = new Database(":memory:", (err) => {
+export const db = new Database("./db/tokens.db", (err) => {
   if (err) {
     console.error(err.message);
     return;
