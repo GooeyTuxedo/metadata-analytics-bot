f66ac13ec1436ed1e86ec17d1daddf31a75703b9
GooeyTuxedo <enderhex@protonmail.com>
2023-03-13 12:53:08 -0700

    fix: commands dir path error in deploy script

--- src/scripts/deploy-commands.ts
+++ src/scripts/deploy-commands.ts
@@ -11,12 +11,12 @@ const [ guildId ] = process.argv.slice(2);
 
 const commands: ApplicationCommand[] = [];
 // Grab all the command files from the commands directory you created earlier
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, '../commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = require(`../commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
